9b6e4bcd9d016d2dbdb858fefb48a25a76813af7
aryanssps <74345307+aryanssps@users.noreply.github.com>
2021-05-15 16:40:18 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "MUMBAI"
+const accessCode2 = "EIGHT";
+const accessCode3 = "JASPRIT BUMRAH";
+
+function clues() {
+    
+    fill(0)
+    textSize(15)
+    text("U B A M M I", 100,50)
+    fill("green")
+    text("Who won ipl 2019 ?" ,100,70)
+
+    fill(0)
+    textSize(15)
+    text("G H E T I", 700,150)
+    fill("green")
+    text("How many teams are in ipl ?", 700,170)
+
+    fill(0)
+    textSize(15)
+    text("SPTIRJA MUBARH", 100,250)
+    fill("green")
+    text("Who was the man of match in ipl 2020 ?", 100,270)
+
+}
